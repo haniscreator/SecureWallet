259666f59e5bb1e0276289a1abb78330d2a742c2
haniscreator <han.is.creator@gmail.com>
2026-01-28 15:43:08 +0700

    fixed at api route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,19 +1,21 @@
+<?php
+
 use App\Http\Controllers\Api\V1\Auth\AuthController;
 use App\Http\Controllers\Api\V1\User\MemberController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-// Auth
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    // Auth
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-// User / Members
-Route::middleware('auth:sanctum')->group(function () {
-Route::get('/user', function (Request $request) {
-return $request->user();
-});
+    // User / Members
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
 
-Route::post('/members', [MemberController::class, 'store']);
-});
+        Route::post('/members', [MemberController::class, 'store']);
+    });
 });
